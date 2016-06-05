6f64e6cfce160e8db1972a93b8467b51ec99d3cd
William Blythe <wbwilliam7@gmail.com>
2016-06-06 09:46:56 +1000

    prettifying code

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ mb.on('ready', function ready() {
     } = electron;
     // Keep a global reference of the window object, if you don't, the window will
     // be closed automatically when the JavaScript object is garbage collected.
-let win = BrowserWindow;  // BrowserWindow in which to show the dialog
-const {dialog} = require('electron');
+    let win = BrowserWindow; // BrowserWindow in which to show the dialog
+    const { dialog } = require('electron');
 
 
 
