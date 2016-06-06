4dd4f8ef2eaf3e2b4fc7e3ee145499b63d3ce1e8
William Blythe <wbwilliam7@gmail.com>
2016-06-06 18:19:36 +1000

    see extended
    added console log for when image is clicked,  added some comments,
    fixed up formatting

--- main.js
+++ main.js
@@ -9,9 +9,7 @@ mb.on('ready', function ready() {
     const electron = require('electron');
     // Module to control application life.
 
-    const {
-        app
-    } = electron;
+    const {app} = electron;
     // Module to create native browser window.
     const {
         BrowserWindow
