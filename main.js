aefd0913422a738bcbbb5135253edf86399d0a58
William Blythe <wbwilliam7@gmail.com>
2016-08-02 08:28:14 +1000

    update highlight to be only on selection thanks to new api

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ mb.on('ready', function ready () {
     // Emitted when the window is closed.
     win.on('closed', () => {
       // Dereference the window object, usually you would store windows
+      win.tray.setHighlightMode(selection);
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
       win = null
