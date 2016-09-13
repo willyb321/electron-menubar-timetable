c2027c58f97d0c14efbeeb4db26007a4df211eb6
William Blythe <wbwilliam7@gmail.com>
2016-09-13 17:48:40 +1000

    did some cleaning up, xo is linter now

--- theJS.js
+++ theJS.js
@@ -1,34 +1,34 @@
 // requires be here
-let remote = require('electron').remote
-let dialog = remote.require('electron').dialog
-let image = document.getElementById('imgTT')
-const Config = require('electron-config')
-const conf = new Config()
+var path;
+let remote = require('electron').remote;
+let dialog = remote.require('electron').dialog;
+let image = document.getElementById('imgTT'); // eslint-disable-line
+const Config = require('electron-config');
+const conf = new Config();
 if (conf.get('imgPath') === undefined) {
-	var path = dialog.showOpenDialog({
+	path = dialog.showOpenDialog({
 		properties: ['openFile', 'multiSelections']
-	})
+	});
 }
-conf.set('imgPath', path)
+conf.set('imgPath', path);
 
 function changeImg() { // find the image, and then change the src to the path.
 	if (image.src.match(conf.get('imgPath')[0])) {
-		image.src = conf.get('imgPath')[1]
-		console.log('Swapped to image 1')
+		image.src = conf.get('imgPath')[1];
+		console.log('Swapped to image 1');
 	} else {
-		image.src = conf.get('imgPath')[0]
-		console.log('Swapped to image 2')
+		image.src = conf.get('imgPath')[0];
+		console.log('Swapped to image 2');
 	}
-
 }
 
 function clearConf() { //eslint-disable-line
-	conf.delete('imgPath')
+	conf.delete('imgPath');
 	var path = dialog.showOpenDialog({
 		properties: ['openFile', 'multiSelections']
-	})
-	conf.set('imgPath', path)
-	changeImg()
+	});
+	conf.set('imgPath', path);
+	changeImg();
 }
-console.log(conf.get('imgPath'))
-changeImg()
+console.log(conf.get('imgPath'));
+changeImg();
