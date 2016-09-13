e507772543f8efe7ff99982275edae044af36304
willyb321 <wbwilliam7@gmail.com>
2016-09-13 12:44:53 +1000

    tab master race:

--- theJS.js
+++ theJS.js
@@ -5,27 +5,30 @@ let image = document.getElementById('imgTT')
 const Config = require('electron-config')
 const conf = new Config()
 if (conf.get('imgPath') === undefined) {
-  var path = dialog.showOpenDialog({
-    properties: ['openFile', 'multiSelections']
-  }) }
-conf.set('imgPath', path)
-function changeImg () {
-  // find the image, and then change the src to the path.
-  if (image.src.match(conf.get('imgPath')[0])) {
-    image.src = conf.get('imgPath')[1]
-    console.log('Swapped to image 1')
-  } else {
-    image.src = conf.get('imgPath')[0]
-    console.log('Swapped to image 2')
-  }
+	var path = dialog.showOpenDialog({
+		properties: ['openFile', 'multiSelections']
+	})
 }
-function clearConf () { //eslint-disable-line
-  conf.delete('imgPath')
-  var path = dialog.showOpenDialog({
-    properties: ['openFile', 'multiSelections']
-  })
-  conf.set('imgPath', path)
-  changeImg()
+conf.set('imgPath', path)
+
+function changeImg() { // find the image, and then change the src to the path.
+	if (image.src.match(conf.get('imgPath')[0])) {
+		image.src = conf.get('imgPath')[1]
+		console.log('Swapped to image 1')
+	} else {
+		image.src = conf.get('imgPath')[0]
+		console.log('Swapped to image 2')
+	}
+
+}
+
+function clearConf() { //eslint-disable-line
+	conf.delete('imgPath')
+	var path = dialog.showOpenDialog({
+		properties: ['openFile', 'multiSelections']
+	})
+	conf.set('imgPath', path)
+	changeImg()
 }
 console.log(conf.get('imgPath'))
 changeImg()
